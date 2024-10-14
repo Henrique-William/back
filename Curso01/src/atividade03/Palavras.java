766ea7bf9523196efc9dceb1751b98ef0eaf4574
Henrique William <os.henriquewilliam@gmail.comm>
2024-10-14 16:08:05 -0300

    curso de Java (Screen Match)- Aula 02

---
+++ Curso01/src/atividade03/Palavras.java
@@ -0,0 +1,12 @@
+package atividade03;
+
+public class Palavras{
+    public static void main(String[] args) {
+        char letra = 'A';
+        String palavra = "MARELO";
+
+        String mensagem = "A letra é " + letra + " e a palavra é " + palavra;
+
+        System.out.println(mensagem);
+    }
+}
