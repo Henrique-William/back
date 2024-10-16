6dc5701fdbd713de95fc72632552563c5f85cb15
Henrique William <os.henriquewilliam@gmail.comm>
2024-10-16 16:52:02 -0300

    Aula 03 - Curso de Java (POO)

--- java/curso02/screenmatch/src/Principal.java
+++ java/curso02/screenmatch/src/Principal.java
@@ -1,21 +1,45 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
-        Filme favorito = new Filme();
+        Filme meuFilme = new Filme();
 
-        favorito.setNome("The Matrix");
-        favorito.setAnoDeLancamento(1999);
-        favorito.setDuracaoEmMinutos(135);
-        favorito.setIncluidoNoPlano(true);
+        meuFilme.setNome("The Matrix");
+        meuFilme.setAnoDeLancamento(1999);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
+        System.out.println("Duração do Filme: " + meuFilme.getDuracaoEmMinutos());
 
-        favorito.exibeFichaTecnica();
-        favorito.avalia(9);
-        favorito.avalia(8);
-        favorito.avalia(9);
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(9);
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
 
-        System.out.println("Média de avaliações do filme: " +favorito.pegaMedia());
+        System.out.println("Média de avaliações do filme: " +meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(50);
+
+        Filme outroFilme = new Filme();
+
+        outroFilme.setNome("Avatar");
+        outroFilme.setAnoDeLancamento(2023);
+        outroFilme.setDuracaoEmMinutos(200);
+        outroFilme.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(outroFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
     }
-
 }
